5996ded0dff5020cbffeaf81b4c7571c4bc546ee
eu <brunosl@usp.br>
2021-07-20 22:33:01 -0300

    Proj6 - must do sigmoid at Bpart

--- Entrega2/Projeto6/BPart/proj6B.py
+++ Entrega2/Projeto6/BPart/proj6B.py
@@ -4,31 +4,210 @@ import platform
 import numpy as np
 import cv2
 from numpy.core.fromnumeric import nonzero
-
-xTrain, yTrain = loadlocal_mnist(
-            images_path='train-images.idx3-ubyte', 
-            labels_path='train-labels.idx1-ubyte')
-
-print(xTrain.shape)
-
-x = xTrain.reshape(60000, 28, 28)
+import pandas as pd
+import seaborn as sns
 
 
-xTest = xTrain[int(xTrain.shape[0]/2),-1]
-xTrain = xTrain[0, int(xTrain.shape[0]/2)]
 
-print(xTest.shape)
-print(xTrain.shape)
+def loadSet(names):
+    images, labels = loadlocal_mnist(
+                images_path=names[0], 
+                labels_path=names[1])
+
+    images = images.reshape(60000, 28, 28)
+
+    imagesTest = images[int(images.shape[0]/2): -1]
+    labelsTest = labels[int(labels.shape[0]/2): -1]
+
+    imagesTrain = images[0:int(images.shape[0]/2)]
+    labelsTrain = labels[0:int(labels.shape[0]/2)]
+
+    digitsTrain, numberOfEachDigitTrain = np.unique(labelsTrain, return_counts=1)
+    digitsTest, numberOfEachDigitTest = np.unique(labelsTest, return_counts=1)
+    
+    digitsSortedTrain = np.argsort(labelsTrain)
+    digitsSortedTest = np.argsort(labelsTest)
+
+    sortedTrain = imagesTrain[digitsSortedTrain]
+    sortedTest = imagesTest[digitsSortedTest]
 
 
-digits, numberOfEachDigit = np.unique(yTrain, return_counts=1)
+    return [sortedTrain, labelsTrain[digitsSortedTrain], sortedTest, labelsTest[digitsSortedTest],  digitsTrain, numberOfEachDigitTrain, digitsTest, numberOfEachDigitTest ]
 
-# print(digits)
-print(numberOfEachDigit)
 
-digitsSorted = np.argsort(yTrain)
+def meanSet(digits, numberOfEachDigit ,sortedTrain):
+    general = []
 
-imagesSorted = x[digitsSorted]
+    last = 0
+
+    lastIndex = 0
+
+    for i in range(len(digits)):
+        mean = np.zeros(sortedTrain[0].shape)
+        lastIndex = last
+        # print("lastIndex " + str(lastIndex))
+        for j in range(lastIndex, lastIndex+numberOfEachDigit[i]):
+            mean += sortedTrain[j]
+            last = j
+        general.append(mean/numberOfEachDigit[i])
+        if(i==0):
+            erosion_size = 1
+            element = cv2.getStructuringElement(cv2.MORPH_CROSS, (2 * erosion_size + 1, 2 * erosion_size + 1),(erosion_size, erosion_size))
+            general[i] = cv2.erode(general[i], element)
+        # plt.imshow(general[-1], cmap="gist_gray")
+        # plt.show()
+    print("Average calculated with " + str(len(sortedTrain)) + " images")
+    return general
+
+
+###Creating weights
+def createWeights(general):
+    weights = []
+    for i in range(len(general)):
+        weights.append(np.hstack(general[i]))
+
+    return weights
+
+def createStimulus(images):
+    stim = []
+    for i in range(len(images)):
+        stim.append(np.hstack(images[i]))
+
+    return stim
+
+def norm(vector):
+    norm = []
+    for i in range(len(vector)):
+        norm.append(np.linalg.norm(vector[i]))
+    return norm
+
+def projection(weights, stim, labelsTest):
+
+    guess = []
+
+    for i in range(len(stim)):
+        proj = []
+        for j in range(len(weights)):
+            proj.append(np.dot(stim[i], weights[j]))
+        #print(proj)
+        ansatz = np.argmax(proj)
+        guess.append([ansatz, labelsTest[i]])
+    #print(guess)
+    return guess
+
+def createConfusionMat(guessed,numberOfEachDigitTest):
+    a = np.zeros((10,10))
+    for i in range(len(guessed)):
+
+        a[guessed[i][0], guessed[i][1]] += 1
+    # a = np.round(a, decimals=1)
+    # print(numberOfEachDigitTest[8])
+    # for i in range(len(a)):
+    #     a[i:] = a[i:]/numberOfEachDigitTest
+
+
+    # aMax = a.max()
+    # aMin = a.min()
+
+    # aNorm = (a - aMin ) / (aMax - aMin)
+    
+    df_cm = pd.DataFrame(a, index = [i for i in "0123456789"],columns = [i for i in "0123456789"])
+    #plt.figure(figsize = (10,7))
+    sns.heatmap(df_cm, annot=True, cmap="gist_gray",  fmt='g', linewidths=.5, cbar=False)
+    plt.xlabel("Real") 
+    plt.ylabel("Inferido")
+    plt.tick_params(axis='both', which='major', labelsize=10, labelbottom = False, bottom=False, top = False, labeltop=True)
+    plt.title("noErodeZero")
+    plt.show()
+    '''
+    To normalize:
+    zi=xi−min(x)/max(x)−min(x)
+    '''
+
+def chooseOne(sortedTrain, numberOfEachDigitTest):
+
+    choosen = []
+    last = 0
+    
+    for i in range(len(numberOfEachDigitTrain)):
+        index = 0
+        
+        if(i == 0):
+            choosen.append(sortedTrain[i])
+        
+        else:
+            for j in range(0, i):
+                index += numberOfEachDigitTrain[j]
+            choosen.append(sortedTrain[index])
+        
+        # plt.imshow(choosen[-1])
+        # plt.show()
+
+    return choosen
+
+def sigmoidDiscrim(weights, stim, labelsTest):
+
+    guess = []
+    
+    
+
+    for i in range(len(stim)):
+        proj = []
+        s = []
+        out = []
+        for j in range(len(weights)):
+            s.append(np.dot(stim[i], weights[j]))
+        
+        a = max(s)
+        b = min(s)
+
+        aNorm = (s - b ) / (a - b)
+        
+        for i in range(len(aNorm)):
+            out.append(sigmoidFunc(aNorm[i], 5))
+        
+        if(i == 2963):
+            print(aNorm)
+            print(out)
+            plt.plot(aNorm, out)
+            plt.xlabel("In(S)")
+            plt.ylabel("Out(A(S))")
+            plt.show()
+            #scatter.append([dotProd, thresh])
+        #     if (dotProd>= thresh):
+        #         proj.append(dotProd)
+        #     else:
+        #         proj.append(0)
+        # #print(proj)
+        
+        #ansatz = np.argmax(proj)
+        #guess.append([ansatz, labelsTest[i]])
+    #print(scatter)
+    
+    return guess
+
+def sigmoidFunc(dot, b):
+    
+    return (1/(1+np.exp(-b*dot)) - 0.5)
+
+def sigmoid(x, b):
+    #a = 1/(1+np.exp(-b*x))
+    
+    for i in range(0, 20, 3):
+        a = (1/(1+np.exp(-i*x)) - 0.5) #* 1/110
+        plt.plot(x, a, label=r'$\beta = $' + str(i))
+    #plt.xlim([-5, 5])
+    #plt.ylim([-5, 5])
+    plt.legend()
+    plt.xlabel("In (S)")
+    plt.ylabel("Out")
+    plt.title("Sigmoid "r'$P = A_{(s)} = \frac{1}{1+\exp^{-s\beta}} - 0.5$')
+    plt.grid()
+    plt.show()
+
+# x = np.round(np.arange(-1.1, 1.1, 0.01), 2)
+
+# sigmoid(x, 15.5)
 
 ####Number of digits-1 is de index of the last of each digit image
 # plt.imshow(imagesSorted[5922])#x[digitsSorted[i]]
@@ -36,35 +215,83 @@ imagesSorted = x[digitsSorted]
 # plt.imshow(imagesSorted[5923])
 # plt.show()
 
-general = []
 
-last = 0
+names = ["train-images.idx3-ubyte", "train-labels.idx1-ubyte"]
 
-lastIndex = 0
+sortedTrain, labelsTrain, sortedTest, labelsTest, digitsTrain, numberOfEachDigitTrain, digitsTest, numberOfEachDigitTest  = loadSet(names)
 
-erosion_size = 1
-element = cv2.getStructuringElement(cv2.MORPH_CROSS, (2 * erosion_size + 1, 2 * erosion_size + 1),(erosion_size, erosion_size))
+print(numberOfEachDigitTest)
 
-for i in range(len(digits)):
-    mean = np.zeros(imagesSorted[0].shape)
-    lastIndex = last
-    # print("lastIndex " + str(lastIndex))
-    for j in range(lastIndex, lastIndex+numberOfEachDigit[i]):
-        mean += imagesSorted[j]
-        last = j
-    general.append(mean/numberOfEachDigit[i])
-    print(np.count_nonzero(general[i]))
+average = meanSet(digitsTrain, numberOfEachDigitTrain , sortedTrain)
+
+stimulus = createStimulus(sortedTest)
+
+choosen = chooseOne(sortedTrain,numberOfEachDigitTrain)
+
+weights = createWeights(average)
+
+#guessed = projection(weights, stimulus, labelsTest)
+
+guessedSig = sigmoidDiscrim(weights, stimulus, labelsTest)
+
+#createConfusionMat(guessed, numberOfEachDigitTest)
+
+createConfusionMat(guessedSig, numberOfEachDigitTest)
 
 
-# for i in range(len(digits)):
-#     plt.imshow(general[i])
-#     plt.show()
 
-###Creating weights
-weights = []
-for i in range(len(digits)):
-    weights.append(np.hstack(general[i]))
+###Confusion Matrix
+# array = [[33,2,0,0,0,0,0,0,0,1,3], 
+#         [3,31,0,0,0,0,0,0,0,0,0], 
+#         [0,4,41,0,0,0,0,0,0,0,1], 
+#         [0,1,0,30,0,6,0,0,0,0,1], 
+#         [0,0,0,0,38,10,0,0,0,0,0], 
+#         [0,0,0,3,1,39,0,0,0,0,4], 
+#         [0,2,2,0,4,1,31,0,0,0,2],
+#         [0,1,0,0,0,0,0,36,0,2,0], 
+#         [0,0,0,0,0,0,1,5,37,5,1], 
+#         [3,0,0,0,0,0,0,0,0,39,0], 
+#         [0,0,0,0,0,0,0,0,0,0,38]]
+# df_cm = pd.DataFrame(array, index = [i for i in "ABCDEFGHIJK"],
+#                   columns = [i for i in "ABCDEFGHIJK"])
+# plt.figure(figsize = (10,7))
+# sns.heatmap(df_cm, annot=True)
 
+
+'''
+
+###checkNorm
+# avgNorm = norm(average)
+
+# choosenNorm = norm(choosen)
+
+# weightsNorm = norm(weights)
+
+# print("avgNorm")
+# print(avgNorm)
+
+# print("weightsNorm")
+# print(weightsNorm)
+
+# print("choosenNorm")
+# print(choosenNorm)
+
+'''
+
+'''
+###Visualization
+for i in range(len(average)):
+    plt.imshow(average[i])
+    plt.show()
+
+for i in range(len(weights)):
+    cv2.imshow("str(i)", weights[i])
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+'''
+
+# erosion_size = 1
+# element = cv2.getStructuringElement(cv2.MORPH_CROSS, (2 * erosion_size + 1, 2 * erosion_size + 1),(erosion_size, erosion_size))
 
 
 #imgTest = np.hstack(imagesSorted[])
